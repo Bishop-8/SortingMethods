7e51e1f8b71b86366dbf0fa50028603d136ff61b
Bishop-8 <80771910+Bishop-8@users.noreply.github.com>
2021-12-11 14:58:07 -0500

    Update MakeArray.py

--- MakeArray.py
+++ MakeArray.py
@@ -1,1 +1,9 @@
-Code to make unsorted Array!
+#Code to make unsorted Array
+import array
+import numpy as np
+
+def MakeArray():
+  arr = array('i', [np.random.randint(100, size=50)]
+  return arr
+
+print(makeArray())
